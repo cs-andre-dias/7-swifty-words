8acb1dcc7718142a2180d885016a784ac309eca8
Andre Dias <andre.dias@concrete.com.br>
2016-10-03 15:58:38 -0300

    Adiciona arquivos iniciais do projeto

---
+++ 7-Swifty-Words/7-Swifty-Words/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  7-Swifty-Words
+//
+//  Created by Andre Dias on 03/10/16.
+//  Copyright © 2016 Andre Dias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
