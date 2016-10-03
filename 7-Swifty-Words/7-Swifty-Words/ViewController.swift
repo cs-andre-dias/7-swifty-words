d110e7b249bedb2289dc4c833441c21b5719f5b8
Andre Dias <andre.dias@concrete.com.br>
2016-10-03 17:03:55 -0300

    É feito todo o layout no storyboard

--- 7-Swifty-Words/7-Swifty-Words/ViewController.swift
+++ 7-Swifty-Words/7-Swifty-Words/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cluesLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var currentAnswer: UITextField!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    @IBAction func submitTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func clearTapped(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
