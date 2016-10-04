c2fca569f2e1c909c2ab45249d72b0d149a24cc5
Andre Dias <andre.dias@concrete.com.br>
2016-10-04 16:30:37 -0300

    Finaliza o jogo

--- 7-Swifty-Words/7-Swifty-Words/ViewController.swift
+++ 7-Swifty-Words/7-Swifty-Words/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GameplayKit
 
 class ViewController: UIViewController {
 
@@ -15,15 +16,111 @@ class ViewController: UIViewController {
     @IBOutlet weak var currentAnswer: UITextField!
     @IBOutlet weak var scoreLabel: UILabel!
     
-    @IBAction func submitTapped(_ sender: UIButton) {
+    @IBAction func submitTapped(_ sender: AnyObject) {
+        if let solutionPosition = solutions.index(of: currentAnswer.text!) {
+            activedButton.removeAll()
+            
+            var splitClues = answerLabel.text!.components(separatedBy: "\n")
+            splitClues[solutionPosition] = currentAnswer.text!
+            answerLabel.text = splitClues.joined(separator: "\n")
+            
+            currentAnswer.text = ""
+            score += 1
+            
+            if score % 7 == 0 {
+                let ac = UIAlertController(title: "Well done!", message: "Are you ready for the next level?", preferredStyle: .alert)
+                ac.addAction(UIAlertAction(title: "Let's go!", style: .default, handler: levelUp))
+                present(ac, animated: true)
+            }
+        }
     }
     
-    @IBAction func clearTapped(_ sender: UIButton) {
+    @IBAction func clearTapped(_ sender: AnyObject) {
+        currentAnswer.text = ""
+        
+        for btn in activedButton {
+            btn.isHidden = false
+        }
+        
+        activedButton.removeAll()
     }
+    
+    var letterButtons = [UIButton]()
+    var activedButton = [UIButton]()
+    var solutions = [String]()
+    
+    var score: Int = 0{
+        didSet {
+            scoreLabel.text = "Score: \(score)" 
+        }
+    }
+    var level = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        for subview in view.subviews where subview.tag == 1001{
+            let btn = subview as! UIButton
+            letterButtons.append(btn)
+            btn.addTarget(self, action: #selector(letterTapped), for: .touchUpInside)
+        }
+        
+        loadLevel()
+    }
+    
+    func loadLevel(){
+        var clueString = ""
+        var solutionString = ""
+        var letterBits = [String]()
+        
+        if let levelFilePath = Bundle.main.path(forResource: "level\(level)", ofType: "txt"){
+            if let levelContents = try? String(contentsOfFile: levelFilePath){
+                var lines = levelContents.components(separatedBy: "\n")
+                lines = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: lines) as! [String]
+                
+                for (index, line) in lines.enumerated(){
+                    let parts = line.components(separatedBy: ": ")
+                    let answer = parts[0]
+                    let clue = parts[1]
+                    
+                    clueString += "\(index + 1). \(clue)\n"
+                    
+                    let solutionWord = answer.replacingOccurrences(of: "|", with: "")
+                    solutionString += "\(solutionWord.characters.count) letters\n"
+                    solutions.append(solutionWord)
+                    
+                    let bits = answer.components(separatedBy: "|")
+                    letterBits += bits
+                }
+            }
+        }
+        cluesLabel.text = clueString.trimmingCharacters(in: .whitespacesAndNewlines)
+        answerLabel.text = solutionString.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        letterBits = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: letterBits) as! [String]
+        
+        if letterBits.count == letterButtons.count {
+            for i in 0 ..< letterBits.count{
+                letterButtons[i].setTitle(letterBits[i], for: .normal)
+            }
+        }
+    }
+    
+    func levelUp(action: UIAlertAction!) {
+        level += 1
+        solutions.removeAll(keepingCapacity: true)
+        
+        loadLevel()
+        
+        for btn in letterButtons {
+            btn.isHidden = false
+        }
+    }
+    
+    func letterTapped(btn: UIButton){
+        currentAnswer.text = currentAnswer.text! + btn.titleLabel!.text!
+        activedButton.append(btn)
+        btn.isHidden = true
     }
 
     override func didReceiveMemoryWarning() {
